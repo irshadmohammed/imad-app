9d8c5e7f5e356088da56b48957bbbe49ad270110
irshadmohammed <30858487+irshadmohammed@users.noreply.github.com>
2017-08-12 05:52:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function() {
+    counetr = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counetr.toString();
+}
